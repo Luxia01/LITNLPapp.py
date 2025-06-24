4139319ea82bcdeaa54dd2908c4504f38b5de72d
Luxia01 <168320986+Luxia01@users.noreply.github.com>
2025-06-24 18:29:41 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,20 @@
-from transformers import AutoTokenizer, AutoModel
 import torch
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import umap
 
-def load_model_and_tokenizer(model_name="distilbert-base-uncased"):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModel.from_pretrained(model_name)
+@torch.no_grad()
+def load_model_and_tokenizer(hf_model):
+    tokenizer = AutoTokenizer.from_pretrained(hf_model)
+    model = AutoModelForSequenceClassification.from_pretrained(hf_model)
+    model.eval()
     return tokenizer, model
 
+@torch.no_grad()
 def compute_embeddings(texts, tokenizer, model):
     inputs = tokenizer(texts, return_tensors="pt", padding=True, truncation=True)
-    with torch.no_grad():
-        outputs = model(**inputs)
-    embeddings = outputs.last_hidden_state[:, 0, :]  # 取 [CLS] 向量
-    return embeddings
+    outputs = model(**inputs, output_hidden_states=True)
+    cls_embeddings = outputs.hidden_states[-1][:, 0, :].numpy()
+    reducer = umap.UMAP()
+    reduced = reducer.fit_transform(cls_embeddings)
+    return reduced
